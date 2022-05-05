ad47b251d0ba9d7f7baf34f75ac69a4f18850e39
Momo <Motazkasem@icloud.com>
2022-05-05 16:51:01 +0200

    Lade till enhetstest på stack isEmpty

--- src/stack.js
+++ src/stack.js
@@ -17,3 +17,16 @@ exports.peek = function () {
     //return stack[0]; // Det här är medvetet felaktigt
     return _.last(stack);
 }
+
+//returns true or false based if the stack has any element 
+exports.isEmpty = function () {
+
+    //Should return false because two elements are added
+    stack.push('motaz')
+    stack.push(12)
+
+    if (stack.length === 0)
+        return true
+    
+    return false
+}
